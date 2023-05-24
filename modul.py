16efa6ef4ad1f4e4d0ac20393defcf216a4f0fea
k789duck <134285370+k789duck@users.noreply.github.com>
2023-05-25 01:11:37 +0900

    Update modul.py

--- modul.py
+++ modul.py
@@ -1,25 +1,15 @@
 def shopping(shop_file):
     shop_dict = {} # 생성할 사전 객체
-    with open(f"./data/{shop_file}") as f:
-    
-        for line in f:
-            words = line.strip().split()
-            if len(words) == 2:
-                menu, price_str = words
-                price_str = price_str.replace("원", "")
 
-                if menu != "#쇼핑몰":
-                    try:
-                        price = int(price_str)
-                    except ValueError:
-                        print(f'Invalid price:{price_str}')
-
-                    shop_dict[menu] = price
+    with open(data_path / shop_file, mode='r', encoding='utf-8') as f:    
+      for line in f:
+        price_of_goods = line.strip().split() 
+        if price_of_goods != []:      
+          goods, price = price_of_goods 
+        if price != shop_file[4]:
+          shop_dict[goods] = int(price.rstrip('원'))
 
     return shop_dict
 
 def item_price(shop_file, item):
-    shopping_list = shopping(shop_file)
-    shopping_price = shopping_list.get(item)
-
-    return shopping_price
+    return shopping(shop_file)[item]
